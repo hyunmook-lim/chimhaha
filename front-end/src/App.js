f7df212b897d8be8e3b73954c0f2b0e332ea05d0
hyunmook-lim <hmlim0320@naver.com>
2022-07-13 18:38:57 +0900

    connect front and back end with axios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,8 @@ import theme from "./data/theme";
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import axios from 'axios';  // 백엔드와 프론트엔드 사이에서 통신을 쉽게 할 수 있도록 도와주는 라이브러리
+
 const View = styled.div`
   display: flex;
   width: 100vw;
