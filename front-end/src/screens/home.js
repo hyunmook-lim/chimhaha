8a9fa21e52ee09787ae28bf4344d328659fcfcc6
yookjungsoo <codepam2020@gmail.com>
2022-08-02 00:40:35 +0900

    Update home.js

--- front-end/src/screens/home.js
+++ front-end/src/screens/home.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import HomeBody from "./home/home_body";
 import { Header, MenuBar, Footer } from "./components";
+import axios from "axios";
 
 const View = styled.div`
   display: flex;
@@ -10,10 +11,44 @@ const View = styled.div`
   width: 100%;
 `;
 
+const Button = styled.button`
+  width: 120px;
+  height: 60px;
+  background-color: gray;
+  color: white;
+  border: none;
+  font-size: 46px;
+  font-weight: bold;
+  border-radius: 10px;
+`;
+
 export default function Home() {
+  const [info, setInfo] = useState([]);
+  function _click() {
+    fetch(
+      axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+        setInfo(res.data);
+      })
+    );
+  }
+
   return (
     <View>
       <Header />
+      <Button onClick={_click}>Test</Button>
+      {info.map((text) => {
+        return (
+          <div
+            style={{
+              border: "black solid 1px",
+              margin: 5,
+            }}
+          >
+            <p>{text.title}</p>
+            <p>{text.body}</p>
+          </div>
+        );
+      })}
       <MenuBar home={true} />
       <HomeBody />
       <Footer />
